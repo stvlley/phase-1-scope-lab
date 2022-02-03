0b308be4d48dc6e1d2e9c194c859063f9eb43d56
stephen talley <stvlley.amzn@gmail.com>
2022-02-02 20:50:24 -0600

    commit tree 8c03bfde57d077842717333c0b74e914aa1c619e

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution in this file!
+var customerName = "bob" ;
+
+function upperCaseCustomerName() {
+    customerName = (customerName.toUpperCase())
+
+};
+function setBestCustomer() {
+    window.bestCustomer = "not bob";
+};
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob'
+};
+
+const leastFavoriteCustomer = 'maybe bob';
+
+function changeLeastFavoriteCustomer () {
+    leastFavoriteCustomer = "bobby"
+}
